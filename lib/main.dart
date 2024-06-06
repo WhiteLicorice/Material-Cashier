4c8c2215433bc176cd8947f3e809eb914060b18e
RB <rbjocsing@up.edu.ph>
2024-06-06 08:26:52 +0800

    feat. transactions form

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,64 @@
-//	Dart
-import 'package:brm_cashier/transaction/transact_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:responsive_framework/responsive_framework.dart';
+// Dart file for the main entry point of the Flutter application
 
-//	Internals
-import 'utils/_responsive_values.dart';
+// Importing necessary packages and files
+import 'package:brm_cashier/transaction/transact_screen.dart'; // Importing a screen for transaction functionality
+import 'package:flutter/material.dart'; // Importing Flutter's material library for UI components
+import 'package:responsive_framework/responsive_framework.dart'; // Importing a package for making the app responsive
 
+// Internal imports
+import 'utils/_responsive_values.dart'; // Importing utility functions related to responsiveness
+
+// Main function to run the application
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp()); // Running the main application widget
 }
 
+// Main application widget
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false, // Disabling debug banner
+      title: 'Flutter Demo', // Setting the title of the application
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 7, 233, 78),
+          seedColor: const Color.fromARGB(
+              255, 7, 233, 78), // Defining a custom color scheme
         ),
-        useMaterial3: true,
+        useMaterial3: true, // Using Material 3 design
       ),
       builder: (context, child) => ResponsiveBreakpoints.builder(
         child: child!,
         breakpoints: [
-          const Breakpoint(start: 0, end: 450, name: MOBILE),
-          const Breakpoint(start: 451, end: 800, name: TABLET),
-          const Breakpoint(start: 801, end: 1920, name: DESKTOP),
-          const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+          const Breakpoint(
+              start: 0,
+              end: 450,
+              name: MOBILE), // Responsive breakpoints for mobile
+          const Breakpoint(
+              start: 451,
+              end: 800,
+              name: TABLET), // Responsive breakpoints for tablet
+          const Breakpoint(
+              start: 801,
+              end: 1920,
+              name: DESKTOP), // Responsive breakpoints for desktop
+          const Breakpoint(
+              start: 1921,
+              end: double.infinity,
+              name: '4K'), // Responsive breakpoints for 4K screens
         ],
       ),
-      home: const MyHomePage(title: 'Cashier'),
+      home: const MyHomePage(
+          title: 'Cashier'), // Setting the home page of the application
     );
   }
 }
 
+// Home page widget
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title});
 
   final String title;
 
@@ -50,13 +69,15 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    // Getting responsive values for button width and text style
     final buttonWidth = ResponsiveValues.buttonWidth(context);
     final textStyle = ResponsiveValues.textStyle(context);
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: Theme.of(context).primaryColor,
+        title: Text(widget.title), // Setting the title of the app bar
+        backgroundColor:
+            Theme.of(context).primaryColor, // Setting app bar background color
       ),
       body: Center(
         child: Column(
@@ -67,22 +88,23 @@ class _MyHomePageState extends State<MyHomePage> {
                 Navigator.push(
                     context,
                     MaterialPageRoute(
-                        builder: (context) => const TransactScreen()));
+                        builder: (context) =>
+                            const TransactScreen())); // Navigating to transaction screen on button press
               },
               style: ElevatedButton.styleFrom(
-                fixedSize: Size(buttonWidth, 50),
-                textStyle: textStyle,
+                fixedSize: Size(buttonWidth, 50), // Setting fixed button size
+                textStyle: textStyle, // Applying responsive text style
               ),
-              child: const Text("Transact"),
+              child: const Text("Transact"), // Button text
             ),
-            const SizedBox(height: 50),
+            const SizedBox(height: 50), // Adding space between buttons
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {}, // No functionality added to the "Quit" button
               style: ElevatedButton.styleFrom(
-                fixedSize: Size(buttonWidth, 50),
-                textStyle: textStyle,
+                fixedSize: Size(buttonWidth, 50), // Setting fixed button size
+                textStyle: textStyle, // Applying responsive text style
               ),
-              child: const Text("Quit"),
+              child: const Text("Quit"), // Button text
             ),
           ],
         ),
--- lib/transaction/transact_screen.dart
+++ lib/transaction/transact_screen.dart
@@ -1,59 +1,151 @@
 import 'package:flutter/material.dart';
-import '../utils/_responsive_values.dart';
 
-class TransactScreen extends StatelessWidget {
+// Define a StatefulWidget for the TransactScreen
+class TransactScreen extends StatefulWidget {
   const TransactScreen({Key? key}) : super(key: key);
 
   @override
+  State<TransactScreen> createState() => _TransactionScreenState();
+}
+
+// Define the State class associated with TransactScreen
+class _TransactionScreenState extends State<TransactScreen> {
+  // Initialize a list of transactions with dummy data
+  final List<Map<String, dynamic>> transactions = List.generate(10, (index) {
+    return {
+      "id": index,
+      "title": "Transaction #$index",
+      "amount": (index + 1) * 10
+    };
+  });
+
+  // Initialize controllers for text input fields
+  final TextEditingController _transactionController = TextEditingController();
+  final TextEditingController _quantityController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    //final double buttonSize = ResponsiveValues.buttonWidth(context);
-
-    // Mock transactions array
-    List<String> transactions = [
-      'Transaction 1',
-      'Transaction 2',
-      'Transaction 3',
-      'Transaction 4',
-      'Transaction 5',
-    ];
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Transact'),
-        backgroundColor: Theme.of(context).primaryColor,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {
-              //  TODO: Add search functionality here
-            },
+      ),
+      body: Column(
+        children: [
+          // Expanded ListView to display transactions
+          Expanded(
+            child: ListView.builder(
+              itemCount: transactions.length,
+              itemBuilder: (BuildContext context, index) {
+                // Dismissible widget to enable swiping to delete
+                return Dismissible(
+                  key: UniqueKey(),
+                  direction: DismissDirection.endToStart,
+                  onDismissed: (_) {
+                    setState(() {
+                      transactions.removeAt(index);
+                    });
+                  },
+                  background: Container(
+                    color: Colors.red,
+                    margin: const EdgeInsets.symmetric(horizontal: 15),
+                    alignment: Alignment.centerRight,
+                    child: const Icon(
+                      Icons.delete,
+                      color: Colors.white,
+                    ),
+                  ),
+                  // Card widget to display each transaction
+                  child: Card(
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        child: Text(transactions[index]["id"].toString()),
+                      ),
+                      title: Text(transactions[index]["title"]),
+                      subtitle: Text(transactions[index]["amount"].toString()),
+                      trailing: const Icon(Icons.arrow_back),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          // Transaction input form
+          _buildTransactionForm(),
+        ],
+      ),
+    );
+  }
+
+  // Function to build the transaction input form
+  Widget _buildTransactionForm() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          // Text input field for transaction title
+          Expanded(
+            child: TextFormField(
+              controller: _transactionController,
+              decoration: const InputDecoration(
+                labelText: 'Transaction',
+              ),
+            ),
+          ),
+          const SizedBox(width: 16),
+          // Text input field for transaction amount
+          Expanded(
+            child: TextFormField(
+              controller: _quantityController,
+              decoration: const InputDecoration(
+                labelText: 'Amount',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          // Button to add the transaction
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: _addTransaction,
+              child: const Text('Add'),
+            ),
           ),
         ],
       ),
-      body: Center(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width *
-              0.5, // 50% of the screen width
-          child: ListView.builder(
-            itemCount: transactions.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                titleAlignment: ListTileTitleAlignment.center,
-                title: Text(transactions[index]),
-                //  TODO: Add other transaction details here if needed
-              );
-            },
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          //  TODO: Add checkout functionality here
-        },
-        backgroundColor: Colors.green,
-        child: const Icon(Icons.check),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
+  }
+
+  // Function to add a new transaction
+  void _addTransaction() {
+    final transactionTitle = _transactionController.text;
+    final quantityText = _quantityController.text;
+    if (transactionTitle.isNotEmpty && quantityText.isNotEmpty) {
+      // Create a new transaction object and add it to the list
+      final newTransaction = {
+        "id": transactions.length,
+        "title": transactionTitle,
+        "amount": int.parse(quantityText),
+      };
+      setState(() {
+        transactions.add(newTransaction);
+      });
+      // Clear input fields after adding transaction
+      _transactionController.clear();
+      _quantityController.clear();
+    } else {
+      // Show an error message if input fields are empty
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Please fill in both fields.'),
+        ),
+      );
+    }
   }
 }
